c5b72c8a5180456b52ba2e59f0f7ab2a02d26fc4
Meowth52 <meowth@meowth.se>
2015-12-10 15:01:36 +0100

    start
    dag 2

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            input.Focus();
+            input.SelectAll();
+            
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key==Key.Return)
+            {
+                var inputData = input.Text;
+                int resultat =0;
+                int iteration =0;
+                char packetsplitter = '\n';
+                char dimensionsplitter = 'x'; 
+                string[] packets = inputData.Split(packetsplitter);
+                string[] dimensionstring;
+                List<int> dimensions = new List<int>();
+                List<Present> presentList = new List<Present>();
+                foreach (string s in packets)
+                {
+                    if (s !="")
+                    {
+                        dimensionstring = s.Split(dimensionsplitter);
+
+                        foreach (string ss in dimensionstring)
+                        {
+                            dimensions.Add(Int32.Parse(ss));
+                        }
+                        presentList.Add(new Present(dimensions));
+                        dimensions.Clear();
+                        iteration++;
+                    }
+                }
+                foreach (Present p in presentList)
+                {
+                    resultat = resultat + p.PaperAmount();
+                }
+                //foreach (char c in inputData)
+                //{
+                //    if (c=='\n')
+                //    {
+                //        resultat++;
+
+                //        stringlenght = 0;
+                //    }
+                //    stringlenght++;
+                //    iteration++;
+                //}
+                output.Text = resultat.ToString();
+            }
+        }
+    }
+}
---
+++ WpfApplication1/Present.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1
+{
+    class Present
+    {
+        private int x;
+        private int y;
+        private int z;
+        public Present(List<int> dimensions)
+        {
+            dimensions.Sort();
+            this.x = dimensions[0];
+            this.y = dimensions[1];
+            this.z = dimensions[2];
+        }
+        public int PaperAmount()
+        {
+            return x*2+y*2+x*y*z;
+        }
+
+    }
+}
