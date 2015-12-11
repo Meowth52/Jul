a6af8df3653a2d44e1a3e571d8286a02303ececb
Meowth52 <meowth@meowth.se>
2015-12-11 14:47:49 +0100

    dag 3 del 1

---
+++ Dag3/Dag3/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            input.Focus();
+            input.SelectAll();
+            
+        }
+
+        public int[] getDirection(char c)
+        {
+            int[] returnvalue = new int[] {0,0};
+            switch(c)
+            {
+                case '<':
+                    returnvalue[0] = -1;
+                    return returnvalue;
+                case '>':
+                    returnvalue[0] = +1;
+                    return returnvalue;
+                case '^':
+                    returnvalue[1] = +1;
+                    return returnvalue;
+                case 'v':
+                    returnvalue[1] = -1;
+                    return returnvalue;
+                default:
+                    return returnvalue;
+            }            
+        }
+        public int[] getSum(int[] a, int[] b)
+        {
+            int[] returnvalue=new int[]{0,0};
+            returnvalue[0] = a[0] + b[0];
+            returnvalue[1] = a[1] + b[1];
+            return returnvalue;
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key==Key.Return)
+            {
+                var inputData = input.Text;
+                int resultat =1;
+                int iteration =0;
+                bool unique;
+                int[] direction;
+                int[] current = new int[] {0,0};
+                List<int[]> positions = new List<int[]>();
+                positions.Add(current);
+                foreach (char c in inputData)
+                {
+                    iteration++;
+                    direction = getDirection(c);
+                    current = getSum(positions.Last(), direction);
+                    unique = true;
+                    foreach (int[] i in positions)
+                    {
+                        if (i[0] == current[0] && i[1] == current[1])
+                            unique = false;
+                    }
+                    if (unique)
+                    {
+                        resultat++;
+                    }
+                    positions.Add(current);
+                }
+                output.Text = resultat.ToString();
+            }
+        }
+    }
+}
