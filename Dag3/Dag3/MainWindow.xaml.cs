51f84064f1cda7c40c33462d8edc78b3fa99b6ca
Meowth52 <meowth@meowth.se>
2015-12-11 11:24:02 +0100

    3:e

---
+++ Dag3/Dag3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            input.Focus();
+            input.SelectAll();
+            
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key==Key.Return)
+            {
+                var inputData = input.Text;
+                int resultat =0;
+                int iteration =0;
+                int x = 0;
+                int y = 0;
+                int[,] xy = new int[0,0];
+                //List<int> dimensions = new List<int>();
+                foreach (char c in inputData)
+                {
+                    if (c == '<')
+                        xy;
+                    iteration++;
+                }
+                output.Text = resultat.ToString();
+            }
+        }
+    }
+}
