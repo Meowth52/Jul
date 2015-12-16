f53ce518cbb691c1fc6b52202682f76653091c37
Meowth52 <meowth@meowth.se>
2015-12-16 21:47:29 +0100

    Dag 5
    Ändrade sturktur så att alla dagar ligger i den här lösningen

---
+++ AdventOfCode/AdventOfCode/Day5.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day5
+    {
+        private string input;
+        public Day5(string inputArg)
+        {
+             input = inputArg;
+        }
+        public string Result()
+        {
+            var inputData = input;
+            inputData.Replace("\r", "");
+            string[] strings = inputData.Split('\n');
+            int resultat = 0;
+            int resultat2 = 0;
+            int iteration = 0;
+            bool nice = true;
+            foreach (string s in strings)
+            {
+                if (!(s == ""))
+                {
+                    if (stringJudger(s) == nice)
+                        resultat++;
+                }
+            }
+            foreach (string s in strings)
+            {
+                if (!(s == ""))
+                {
+                    if (stringJudger2(s) == nice)
+                        resultat2++;
+                }
+            }
+            return "del 1 blev " + resultat.ToString() + " del 2 blev " + resultat2.ToString();
+        }
+                int day = 5;
+        public bool stringJudger(string suspect)
+        {
+            bool nice = false;
+            int numberOfVowels=0;
+            bool containsDubbelChar = false;
+            bool containsForbiddenString = false;
+            char lastChar=suspect[0];
+            string vowels = "aeiou";
+            bool first = true;
+            List<string> forbiddenStrings = new List<string>();
+            forbiddenStrings.Add("ab");
+            forbiddenStrings.Add("cd");
+            forbiddenStrings.Add("pq");
+            forbiddenStrings.Add("xy");
+
+            foreach (char c in suspect)
+            {
+                if (c == lastChar && !first)
+                    containsDubbelChar = true;
+                lastChar = c;
+                first = false;
+                if (vowels.Contains(c))
+                    numberOfVowels++;
+            }
+            foreach (string s in forbiddenStrings)
+            {
+                if (suspect.Contains(s))
+                    containsForbiddenString = true;
+            }
+            nice = (numberOfVowels >= 3) && containsDubbelChar && !containsForbiddenString;
+            return nice;
+        }
+        public bool stringJudger2(string suspect)
+        {
+            bool nice = false;
+            int numberOfPairs = 0;
+            int charCounter = 0;
+            bool containsDubbelChar = false;
+            int iteration = 0;
+            string suspectChecker = suspect;
+            string PairToBeChecked;
+            foreach (char c in suspect)
+            {
+                if (!((iteration - 2) < 0))
+                {
+                    if (c == suspect[iteration - 2])
+                        containsDubbelChar = true;
+                }
+                if (!(c == '\r'))
+                {
+                    PairToBeChecked = c + suspect[iteration + 1].ToString();
+                    if (suspectChecker.Contains(PairToBeChecked))
+                    {
+                        suspectChecker = suspectChecker.Remove(suspectChecker.IndexOf(PairToBeChecked), 2);
+                        if (suspectChecker.Contains(PairToBeChecked))
+                            charCounter++;
+                        suspectChecker = suspect;
+                    }
+                }
+                iteration++;                
+            }
+            numberOfPairs = charCounter;
+            nice = (numberOfPairs >= 2) && containsDubbelChar;
+            return nice;
+          }  
+    }
+}
--- AdventOfCode/AdventOfCode/MainWindow.xaml.cs
+++ AdventOfCode/AdventOfCode/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Security.Cryptography;
 
 namespace AdventOfCode
 {
@@ -21,96 +20,33 @@ namespace AdventOfCode
     /// </summary>
     public partial class MainWindow : Window
     {
-        public bool stringJudger(string suspect)
-        {
-            bool nice = false;
-            int numberOfVowels=0;
-            bool containsDubbelChar = false;
-            bool containsForbiddenString = false;
-            char lastChar=suspect[0];
-            string vowels = "aeiou";
-            bool first = true;
-            List<string> forbiddenStrings = new List<string>();
-            forbiddenStrings.Add("ab");
-            forbiddenStrings.Add("cd");
-            forbiddenStrings.Add("pq");
-            forbiddenStrings.Add("xy");
-
-            foreach (char c in suspect)
-            {
-                if (c == lastChar && !first)
-                    containsDubbelChar = true;
-                lastChar = c;
-                first = false;
-                if (vowels.Contains(c))
-                    numberOfVowels++;
-            }
-            foreach (string s in forbiddenStrings)
-            {
-                if (suspect.Contains(s))
-                    containsForbiddenString = true;
-            }
-            nice = (numberOfVowels >= 3) && containsDubbelChar && !containsForbiddenString;
-            return nice;
-        }
-        public bool stringJudger2(string suspect)
-        {
-            bool nice = false;
-            int numberOfPairs = 0;
-            int charCounter = 0;
-            bool containsDubbelChar = false;
-            int iteration = 0;
-            foreach (char c in suspect)
-            {
-                if (!((iteration - 2) < 0))
-                {
-                    if (c == suspect[iteration - 2])
-                        containsDubbelChar = true;
-                }
-                if (suspect.Contains(c + suspect[iteration].ToString()))
-                    numberOfPairs++;
-                iteration++;                
-            }
-            numberOfPairs = numberOfPairs + charCounter / 2;
-            nice = (numberOfPairs >= 2) && containsDubbelChar;
-            return nice;
-        }
+        int day = 5;
         public MainWindow()
         {
             InitializeComponent();
             input.Focus();
             input.SelectAll();
+            ValdDag.Content = day.ToString();
         }
 
         private void TextBox_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Return)
             {
-                var inputData = input.Text;
-                inputData.Replace("\r", "");
-                string[] strings = inputData.Split('\n');
-                int resultat = 0;
-                int resultat2 = 0;
-                int iteration = 0;
-                bool nice = true;
-                foreach (string s in strings)
-                {
-                    if (!(s == ""))
-                    {
-                        if (stringJudger(s) == nice)
-                            resultat++;
-                    }
-                }
-                foreach (string s in strings)
-                {
-                    if (!(s == ""))
-                    {
-                        if (stringJudger2(s) == nice)
-                            resultat2++;
-                    }
-                }
-                output.Text = "del 1 blev " + resultat.ToString() + " del 2 blev " + resultat2.ToString();
+                Day5 chosenDay = new Day5(input.Text);
+                output.Text = chosenDay.Result();
             }
+        }
+        private void onClick5(object sender, RoutedEventArgs e)
+        {
+            day = 5;
+            ValdDag.Content = day.ToString();
+        }
+
+        private void onClick6(object sender, RoutedEventArgs e)
+        {
+            day = 6;
+            ValdDag.Content = day.ToString();
         }
     }
 }
